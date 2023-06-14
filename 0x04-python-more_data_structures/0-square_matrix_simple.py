384cbfc604e634cf8b4ed75b27b55f8f30ee10b7
wolfie3d <kwangechi3@gmail.com>
2023-06-14 12:08:16 +0300

    third trial

--- 0x04-python-more_data_structures/0-square_matrix_simple.py
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -1,4 +1,3 @@
-#!/usr/bin/python3 
+#!/usr/bin/python3
 def square_matrix_simple(matrix=[]):
-      return [[elements ** 2 for elements in element] for element in matrix]
-      
+    return [[elements ** 2 for elements in element] for element in matrix]

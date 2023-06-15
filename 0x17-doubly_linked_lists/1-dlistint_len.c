339a6b831bfe149e06a027fc0da7375f4214ce77
wolfie3d <kwangechi3@gmail.com>
2023-06-15 12:40:46 +0300

    edited, with betty

--- 0x17-doubly_linked_lists/1-dlistint_len.c
+++ 0x17-doubly_linked_lists/1-dlistint_len.c
@@ -1,17 +1,17 @@
 #include "lists.h"
 /**
- * dlistint_len - Return number of elements in dll
- * @h: Pointer to struct of ddl
- * Return: The amount of elements in dll
- */
+* dlistint_len - Return number of elements in dll
+* @h: Pointer to struct of ddl
+* Return: The amount of elements in dll
+*/
 size_t dlistint_len(const dlistint_t *h)
 {
-	int count_node = 0;
+int count_node = 0;
 
-	while (h != NULL)
-	{
-		h = h->next;
-		count_node++;
-	}
-	return (count_node);
+while (h != NULL)
+{
+h = h->next;
+count_node++;
+}
+return (count_node);
 }
